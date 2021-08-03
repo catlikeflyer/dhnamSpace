5329091782e2d74055bad213f6fa42d9e9480ad0
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2021-08-03 10:23:02 -0500

    Added Firebase Config

--- src/context.js
+++ src/context.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createContext } from "react";
 import datas from "./data.json";
+import { pFirestore } from "./firebaseConfig";
 
 export const DataContext = createContext();
 
 export function DataProvider({ children }) {
   const data = datas;
 
+  const useFirestore = (collection) => {
+    const [docs, setDocs] = useState([]);
+
+    useEffect(() => {
+      const unsub = pFirestore
+        .collection(collection)
+        .orderBy("date", "desc")
+        .onSnapshot((snapshot) => {
+          let documents = [];
+          snapshot.forEach((doc) => {
+            documents.push({ ...doc.data(), id: doc.id });
+          });
+          setDocs(documents);
+          console.log(documents);
+        });
+
+      return () => unsub();
+    }, [collection]);
+
+    return { docs };
+  };
+
   return (
     <DataContext.Provider
       value={{
         data,
+        useFirestore,
       }}
     >
       {children}
