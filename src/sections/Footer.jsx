7b3b7908779cd296c435dd5d1d5e48ea7c805135
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2021-08-09 14:47:49 -0500

    Updated Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,14 +1,14 @@
-import { Container, Box } from "@material-ui/core";
+import { Box, BottomNavigation } from "@material-ui/core";
 import React from "react";
 
 export default function Footer() {
   return (
-    <Container style={{ backgroundColor: "#a0a0a0", width: "100vw" }}>
+    <BottomNavigation style={{ backgroundColor: "rgba(20, 20, 20, 0.8)", top: 100, color: "white"}}>
       <Box display="flex" alignItems="center" justifyContent="center">
         <span>
           Made with <i class="fa fa-heart pulse"></i> by DHNAM
         </span>
       </Box>
-    </Container>
+    </BottomNavigation>
   );
 }
