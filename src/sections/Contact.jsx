14bf4015fd6b3f98501888505fc1d128d63573f0
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2021-08-09 11:26:00 -0500

    Updated contacts

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,14 +7,15 @@ import {
   useTheme,
   IconButton,
   Icon,
+  Button,
 } from "@material-ui/core";
 import ColorDivider from "../components/ColorDivider";
 import { DataContext } from "../context";
+import { blueGrey } from "@material-ui/core/colors";
 
 export default function Contact() {
   const useStyles = makeStyles((theme) => ({
     container: {
-      backgroundColor: "black",
       width: "100%",
       color: "white",
       minHeight: "100vh",
@@ -25,13 +26,14 @@ export default function Contact() {
       },
     },
     link: {
-        textDecoration: "none",
-        color: "inherit"
-    }
+      textDecoration: "none",
+      color: "inherit",
+    },
   }));
   const theme = useTheme();
   const classes = useStyles(theme);
   const { data } = useContext(DataContext);
+  const blueGray = blueGrey[500]
 
   return (
     <section id="contact">
@@ -42,6 +44,13 @@ export default function Contact() {
           </Typography>
           <ColorDivider />
         </Container>
+        <Typography paragraph align="center" component="h3">
+          Follow me on my social media accounts or send me an{" "}
+          <a href="mailto: dhnam@aol.com" style={{ color: "aliceblue" }}>
+            email
+          </a>
+          !
+        </Typography>
         <Box
           display="flex"
           flexDirection="row"
@@ -65,6 +74,21 @@ export default function Contact() {
               </a>
             </IconButton>
           ))}
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button
+            variant="contained"
+            color={blueGray}
+            className={classes.button}
+            endIcon={<Icon className="fas fa-file-download" />}
+          >
+            Download CV
+          </Button>
         </Box>
       </Box>
     </section>
